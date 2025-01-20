518e27e4309854098b78204fdd678cc65b6a0491
sktanui <shadrackkemei2019@gmail.com>
2025-01-20 11:38:38 +0300

    pass tests and complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,15 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        
+        response = requests.get(self.url)
+        return response.content  # Return the raw response body exactly as a string
 
     def load_json(self):
-        pass
+       
+        response_body = self.get_response_body()  # Get the raw response body as a string
+        return json.loads(response_body)  # Parse the raw string into a Python data structure
